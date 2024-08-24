7e11bf412262cd44fe8fc7c4acc1fed4f1aba1ab
Primary_Writings <User1@mail>
2024-08-24 15:23:57 +0300

    first commit

---
+++ Craph.cs
@@ -0,0 +1,213 @@
+﻿using System;
+
+public class Graph
+{
+    public bool[,] Matrix;
+    public int[,] Value;
+    private List<List<int>> List;
+    public int size;
+
+    public Graph(int size_graph)
+    {
+        size = size_graph;
+        Matrix = new bool[size, size];
+        Value = new int[size, size];
+        List = new List<List<int>>(size);
+        for (int i = 0; i < size; i++)
+        {
+            List.Add(new List<int>());
+        }
+    }
+
+    public void AddVertex()
+    {
+        size++;
+        bool[,] newAdjMatrix = new bool[size, size];
+        int[,] newAdjValue = new int[size, size];
+        List<List<int>> newAdjList = new List<List<int>>(size);
+        for (int i = 0; i < size - 1; i++)
+        {
+            newAdjList.Add(new List<int>());
+            for (int j = 0; j < size - 1; j++)
+            {
+                newAdjMatrix[i, j] = Matrix[i, j];
+                newAdjValue[i, j] = Value[i, j];
+            }
+        }
+        newAdjList.Add(new List<int>());
+        Matrix = newAdjMatrix;
+        Value = newAdjValue;
+        List = newAdjList;
+    }
+
+    public void RemoveVertex(int vertex)
+    {
+        if (vertex < 0 || vertex >= size)
+        {
+
+            return;
+        }
+        size--;
+        bool[,] newAdjMatrix = new bool[size, size];
+        int[,] newAdjValue = new int[size, size];
+        List<List<int>> newAdjList = new List<List<int>>(size);
+        int k = 0;
+        for (int i = 0; i < size + 1; i++)
+        {
+            if (i == vertex)
+            {
+                continue;
+            }
+            newAdjList.Add(new List<int>());
+            int l = 0;
+            for (int j = 0; j < size + 1; j++)
+            {
+                if (j == vertex)
+                {
+                    continue;
+                }
+
+                newAdjMatrix[k, l] = Matrix[i, j];
+                newAdjValue[k, l] = Value[i, j];
+                if (Matrix[i, j])
+                {
+                    newAdjList[k].Add(j);
+                }
+                l++;
+            }
+            k++;
+        }
+        Value = newAdjValue;
+        Matrix = newAdjMatrix;
+        List = newAdjList;
+    }
+
+    public void AddEdge(int start, int end)
+    {
+        Random r = new Random();
+        if (start < 0 || start >= size || end < 0 || end >= size)
+        {
+            Console.WriteLine("Incorrect index");
+            return;
+        }
+
+        Matrix[start, end] = true;
+        List[start].Add(end);
+        Matrix[end, start] = true;
+        List[end].Add(start);
+        Value[start, end] = Value[end, start] = r.Next(1, 10);
+    }
+
+    public void RemoveEdge(int start, int end)
+    {
+        if (start < 0 || start >= size || end < 0 || end >= size)
+        {
+            Console.WriteLine("Incorrect index");
+            return;
+        }
+
+        Matrix[start, end] = false;
+        Value[start, end] = Value[end, start] = 0;
+        List[start].Remove(end);
+    }
+
+
+
+    public void PrintGraph()
+    {
+        Console.WriteLine("Adjacency matrix:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(Matrix[i, j] ? "1 " : "0 ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("list of adjacencies:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"{i}: ");
+            foreach (var vertex in List[i])
+            {
+                Console.Write($"{vertex} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void PrintValue()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(Value[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            writer.WriteLine(size);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    writer.Write(Matrix[i, j] ? "1 " : "0 ");
+                }
+                writer.WriteLine();
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    writer.Write(Value[i, j] + " ");
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            size = int.Parse(reader.ReadLine());
+            Matrix = new bool[size, size];
+            List = new List<List<int>>(size);
+            Value = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                List.Add(new List<int>());
+                string[] line = reader.ReadLine().Split(' ');
+                for (int j = 0; j < size; j++)
+                {
+                    Matrix[i, j] = line[j] == "1";
+                    if (line[j] == "1")
+                    {
+                        List[i].Add(j);
+                    }
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                string[] line = reader.ReadLine().Split(' ');
+                for (int j = 0; j < size; j++)
+                {
+                    Value[i, j] = int.Parse(line[j]);
+
+                }
+            }
+
+        }
+    }
+
+
+}
+
---
+++ Graph_algorithms.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+public class GraphAlg
+{
+	 // DFS
+    private void Depth(int v, bool[] visited)
+    {
+        visited[v] = true;
+        Console.Write(v + " ");
+
+        foreach (int i in List[v])
+        {
+            if (!visited[i])
+            {
+                Depth(i, visited);
+            }
+        }
+    }
+    public void Deth()
+    {
+        bool[] visited = new bool[size];
+        for (int i = 0; i < size; i++)
+        {
+            if (!visited[i])
+            {
+                Depth(i, visited);
+            }
+        }
+    }
+
+    // BFS
+    public void Level(int s)
+    {
+        bool[] visited = new bool[size];
+        int[] queue = new int[size];
+        int Now = 0;
+        int Next = 0;
+        visited[s] = true;
+        queue[Next++] = s;
+
+        while (Now != Next)
+        {
+            s = queue[Now++];
+            Console.Write(s + " ");
+
+            foreach (int i in List[s])
+            {
+                
+                if (!visited[i])
+                {
+                    visited[i] = true;
+                    queue[Next++] = i;
+                }
+            }
+        }
+    }
+
+
+    // Dijkstra-Prim's algorithm
+    private int MinKey(int[] key, bool[] mstSet)
+    {
+        int min = int.MaxValue, minIndex = -1;
+
+        for (int v = 0; v < size; v++)
+        {
+            if (mstSet[v] == false && key[v] < min)
+            {
+                min = key[v];
+                minIndex = v;
+            }
+        }
+        return minIndex;
+    }
+    public void PrimMST()
+    {
+        int[] parent = new int[size];
+        int[] key = new int[size];
+        bool[] mstSet = new bool[size];
+        for (int i = 0; i < size; i++)
+        {
+            key[i] = int.MaxValue;
+            mstSet[i] = false;
+        }
+        key[0] = 0;
+        parent[0] = -1;
+        for (int count = 0; count < size - 1; count++)
+        {
+            int u = MinKey(key, mstSet);
+            mstSet[u] = true;
+
+            for (int v = 0; v < size; v++)
+            {
+                if (Value[u, v] != 0 && mstSet[v] == false && Value[u, v] < key[v])
+                {
+                    parent[v] = u;
+                    key[v] = Value[u, v];
+                }
+            }
+        }
+        Console.WriteLine("Вершины\t Вес");
+        for (int i = 1; i < size; i++)
+        {
+            Console.WriteLine(parent[i] + " - " + i + "\t " + Value[i, parent[i]]);
+        }
+    }
+
+
+    // Kruskal's algorithm
+    private int Find(int[] parent, int i)
+    {
+        while (parent[i] != i)
+        {
+            i = parent[i];
+        }
+        return i;
+    }
+    private void Union(int[] parent, int x, int y)
+    {
+        int x_set = Find(parent, x);
+        int y_set = Find(parent, y);
+        parent[x_set] = y_set;
+    }
+
+    public void Kruskal()
+    {
+        int[] parent = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            parent[i] = i;
+        }
+        int edgeCount = 0;
+        Console.WriteLine("Rib\t Weight");
+        while (edgeCount < size - 1)
+        {
+            int min = int.MaxValue;
+            int a = -1, b = -1;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (Find(parent, i) != Find(parent, j) && Value[i, j] < min && Value[i, j] != 0)
+                    {
+                        min = Value[i, j];
+                        a = i;
+                        b = j;
+                    }
+                }
+            }
+
+            Union(parent, a, b);
+            Console.WriteLine(a + " - " + b + "\t " + Value[a, b]);
+            edgeCount++;
+        }
+    }
+
+
+    //Finding the Shortest Path
+    int MinDistance(int[] dist, bool[] sptSet)
+    {
+        int min = int.MaxValue, minIndex = -1;
+
+        for (int v = 0; v < size; v++)
+        {
+            if (!sptSet[v] && dist[v] <= min)
+            {
+                min = dist[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+    public void Dijkstra(int start, int end)
+    {
+        int[] ist = new int[size];
+        bool[] visited = new bool[size];
+        for (int i = 0; i < size; i++)
+        {
+            ist[i] = int.MaxValue;
+            visited[i] = false;
+        }
+        ist[start] = 0;
+        for (int count = 0; count < size - 1; count++)
+        {
+            int u = MinDistance(ist, visited);
+            visited[u] = true;
+            for (int v = 0; v < size; v++)
+            {
+                if (!visited[v] && Value[u, v] != 0 && ist[u] != int.MaxValue && ist[u] + Value[u, v] < ist[v])
+                {
+                    ist[v] = ist[u] + Value[u, v];
+                }
+            }
+        }
+        Console.WriteLine("The shortest path from " + start + " to " + end + " is: " + ist[end]);
+    }
+
+}
